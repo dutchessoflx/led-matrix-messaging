47ed3dd4dbb35d7ae8410a6063d5761eb1d9d313
Brooke <brooke_vaneerden@hotmail.com>
2021-02-03 11:57:48 +1100

    cloud server for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,12 +13,12 @@ class MessagesController < ApplicationController
     text = @message.message_content.upcase
     # @message.created_at = @message.created_at.strftime('%d %b %Y %H:%M:%S')
     # client = MQTT::Client.connect('mqtt://homeAssistant:4HyIJ02ofOYqWrGkcQ6s@m12.cloudmqtt.com', 19070)
-    MQTT::Client.connect('mqtt://breelamp:ZbMY6$T0*2y0@192.168.0.162') do |c|
-        c.publish('brookeledmatrix', text)
-      end
-      # MQTT::Client.connect('mqtt://homeAssistant:4HyIJ02ofOYqWrGkcQ6s@m12.cloudmqtt.com', 19070) do |c|
-      #     c.publish('brookeledmatrix', text)
-      #   end
+    # MQTT::Client.connect('mqtt://breelamp:ZbMY6$T0*2y0@192.168.0.162') do |c|
+    #     c.publish('brookeledmatrix', text)
+    #   end
+      MQTT::Client.connect('mqtt://homeAssistant:4HyIJ02ofOYqWrGkcQ6s@m12.cloudmqtt.com', 19070) do |c|
+          c.publish('brookeledmatrix', text)
+        end
 
       render json: @message
       # redirect_to messages_path
